93951075c206bff335eab523b68be47f811ce9d0
naamiraa <naamiraaulia@gmail.com>
2024-11-13 14:36:27 +0700

    integrasi Django-Flutter

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main.views import register
 from main.views import login_user
 from main.views import logout_user
 from main.views import edit_mood
-from main.views import delete_mood, add_mood_entry_ajax
+from main.views import delete_mood, add_mood_entry_ajax, create_mood_flutter
 
 
 app_name = 'main'
@@ -23,4 +23,5 @@ urlpatterns = [
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'),
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
 ]
